fd16b24c762ac662c1da2c8a2df8ed2d5816435b
Shivam <89645660+Shivamg27071999@users.noreply.github.com>
2024-03-19 14:43:05 +0530

    Model Building

---
+++ IRIS Model Building MLOPS/app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request
+import numpy as np
+import joblib
+
+app=Flask(__name__)
+####################33
+@app.route('/')
+def home():
+    return render_template("home.html")
+
+
+@app.route('/prediction',methods=['get','post'])
+def prediction():
+    sl=float(request.form.get("slcm"))
+    sw=float(request.form.get("swcm"))
+    pl=float(request.form.get("plcm"))
+    pw=float(request.form.get("pwcm"))
+    data_points=np.array([[sl,sw,pl,pw]])
+
+    model=joblib.load("model/naive_bayes.pkl")
+    prediction=model.predict(data_points)
+    # Get the values from the form in the
+    return render_template("output.html", prediction=prediction)
+
+####################
+
+if __name__ == "__main__":
+    app.run(debug=True,host="0.0.0.0")
